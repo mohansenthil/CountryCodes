e0d5d6d1cda8eb0aa4b5c33d5659cbdb3d050d25
Mohan Raj Senthilnathan <mohanraj.senthilnath@nortonlifelock.com>
2023-04-04 23:23:38 +0530

    Test project

---
+++ TestApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CountryCodes;
+
+namespace TestApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"USA's Two Letter Country Code is {TwoLetterISORegionCode.UnitedStates}");
+            Console.WriteLine($"India's Three letter Country Code is {ThreeLetterISORegionCode.India}");
+        }
+    }
+}
